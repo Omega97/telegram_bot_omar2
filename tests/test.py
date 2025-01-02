1e05b13e07c60950fde5a8957a2784eed6d41f8c
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2025-01-02 22:27:10 +0100

    test architect

--- tests/test.py
+++ tests/test.py
@@ -1,26 +1,12 @@
 from src.architect import Architect
 from src.place import Place
 from scripts.utils import read_file
-import cv2
 import numpy as np
 
 
 def test_show_user_info():
-    architect = Architect(data_dir="..\\data")
+    architect = Architect(data_dir="..\\DATA")
     user_info = architect.get_user_info()
-
-    # architect.set_emoji(156267213, '🟦')  # Omar Cusma Fait '🇸🇮'
-    # architect.set_emoji(473531951, '🟩')  # Peter Cej
-    # architect.set_emoji(890008145, '🇮🇹')  # Christopher 🇮🇹🟨
-    # architect.set_emoji(813074514, '🟥')  # Vittorio
-    # architect.set_emoji(213607266, '🦊')  # Nicolò Rossi
-    # architect.set_emoji(197127934, '🐧')  # Kleit Rrokja
-    # architect.set_emoji(130266190, '🦧')  # Simone Mezzavilla 🟪
-    # architect.set_emoji(231576312, '😺')  # Eleonora Zanetti
-    # architect.set_emoji(6445560359, '🔥')  # Uros
-    # architect.set_emoji(6337524767, '🦊')  # Luis
-    # architect.set_emoji(734566340, '🚀')  # Edo
-    # architect.set_emoji(1474749149, '🦙')  # Tanja
 
     # change user canvas
     architect.set_item(156267213, 'canvas', 'default')  # default, friends
@@ -39,23 +25,12 @@ def test_show_user_info():
     architect.save_user_info()
 
 
-def test_architect():
-    architect = Architect()
-    for user_id in architect.user_info:
-        print()
-        print(user_id)
-        for key, value in architect.user_info[user_id].items():
-            # print key, value, type in gray color
-            print(f'{key}: {value} \033[90m{type(value)}\033[0m')
-    architect.save_user_info()
-
-
 def test_mini_canvas():
     place = Place('maxi', shape=(16, 40))
     print(place)
 
 
-def test_characters(file_path='..\\data\\default_emoji.txt'):
+def test_characters(file_path='..\\DATA\\default_emoji.txt'):
     s = read_file(file_path)
     v = [c for c in s]
     for c in v:
@@ -63,7 +38,7 @@ def test_characters(file_path='..\\data\\default_emoji.txt'):
 
 
 def test_photo():
-    architect = Architect(data_dir="..\\data")
+    architect = Architect(data_dir="..\\DATA")
     names = architect.get_photo_names()
     print(names)
     img = architect.get_photo(names[0])
@@ -75,10 +50,32 @@ def test_photo():
     print(v)
 
 
+def test_architect():
+    architect = Architect(data_dir="..\\DATA")
+    for user_id in architect.get_user_info():
+        print()
+        print(user_id)
+        for key, value in architect.user_info[user_id].items():
+            print(f'{key}: {value} \033[90m{type(value)}\033[0m')
+    architect.save_user_info()
+
+
+def test_architect_2():
+    """Modify user data"""
+    architect = Architect(data_dir="..\\DATA")
+    database = architect.get_user_info()
+    for user_id in database:
+        user_info = database[user_id]
+        print()
+        print(user_id)
+        print(user_info[user_id])
+    architect.save_user_info()
+
 
 if __name__ == '__main__':
     # test_show_user_info()
-    # test_architect()
     # test_mini_canvas()
     # test_characters()
-    test_photo()
+    # test_photo()
+    # test_architect()
+    test_architect_2()
